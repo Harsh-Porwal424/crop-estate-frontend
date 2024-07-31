18fb8513c4ddcae4b63f70dc7a1888e86c6e8210
Harsh Porwal <harshporwal@Harshs-MacBook-Air.local>
2024-08-01 01:07:39 +0530

    update complated

--- src/routes/profileUpdatePage/profileUpdatePage.jsx
+++ src/routes/profileUpdatePage/profileUpdatePage.jsx
@@ -1,16 +1,36 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import "./profileUpdatePage.scss";
 import { AuthContext } from "../../context/AuthContext";
+import apiRequest from "../../lib/apiRequest";
 
 function ProfileUpdatePage() {
-
   const { updateUser, currentUser } = useContext(AuthContext);
+  const [error, setError] = useState(null);
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+
+    const formEntries = Object.fromEntries(formData.entries());
+    const { username, email, password } = formEntries;
+
+    try {
+      const res = await apiRequest.put(`/users/${currentUser.id}`, {
+        username,
+        email,
+        password,
+      });
+      updateUser(res.data);
+    } catch (error) {
+      console.error("Error updating profile:", error);
+      setError(error.response?.data?.message || "An error occurred");
+    }
+  };
 
   return (
     <div className="profileUpdatePage">
       <div className="formContainer">
-        <form>
+        <form onSubmit={handleSubmit}>
           <h1>Update Profile</h1>
           <div className="item">
             <label htmlFor="username">Username</label>
@@ -34,12 +54,17 @@ function ProfileUpdatePage() {
             <label htmlFor="password">Password</label>
             <input id="password" name="password" type="password" />
           </div>
-          <button>Update</button>
+          <button type="submit">Update</button>
         </form>
       </div>
       <div className="sideContainer">
-        <img src={currentUser.avatar || "/noavatar.png"} alt="" className="avatar" />
+        <img
+          src={currentUser.avatar || "/noavatar.png"}
+          alt=""
+          className="avatar"
+        />
       </div>
+      {error && <div className="error">{error}</div>}
     </div>
   );
 }
