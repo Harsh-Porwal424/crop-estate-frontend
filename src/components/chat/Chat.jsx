920fea2a2d074cb36e22fc863f8ac7b58129e779
Harsh Porwal <harshporwal@Harshs-MacBook-Air.local>
2024-08-04 03:34:57 +0530

    chat app

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,26 +1,24 @@
-import { useContext, useState } from "react";
+import { useContext, useState, useEffect } from "react";
 import "./chat.scss";
 import { AuthContext } from "../../context/AuthContext";
 import apiRequest from "../../lib/apiRequest";
 import { format } from "timeago.js";
+import { SocketContext } from "../../context/SocketContext";
 
-function Chat({chats}) {
+function Chat({ chats }) {
   const [chat, setChat] = useState(null);
 
   const { currentUser } = useContext(AuthContext);
+  const { socket } = useContext(SocketContext);
 
   const handleOpenChat = async (id, receiver) => {
-
-    try{
-
-      const response = await apiRequest("/chats/" + id);
-      setChat({...response.data, receiver});
-
-    }catch(err){
-       console.error(err);
+    try {
+      const response = await apiRequest.get("/chats/" + id);
+      setChat({ ...response.data, receiver });
+    } catch (err) {
+      console.error(err);
     }
-
-  }
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -33,65 +31,82 @@ function Chat({chats}) {
       const res = await apiRequest.post("/messages/" + chat.id, { text });
       setChat((prev) => ({ ...prev, messages: [...prev.messages, res.data] }));
       e.target.reset();
+      socket.emit("sendMessage", {
+        receiverId: chat.receiver.id,
+        data: res.data,
+      });
     } catch (err) {
       console.log(err);
     }
   };
 
- // console.log(chats);
+  useEffect(() => {
+    const read = async () => {
+      try {
+        await apiRequest.put("/chats/read/" + chat.id);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    if (chat && chat.id && socket) {
+      socket.on("getMessage", (data) => {
+        if (chat.id === data.chatId) {
+          setChat((prev) => ({ ...prev, messages: [...prev.messages, data] }));
+          read();
+        }
+      });
+
+      return () => {
+        socket.off("getMessage");
+      };
+    }
+  }, [socket, chat]);
+
   return (
     <div className="chat">
       <div className="messages">
         <h1>Messages</h1>
-        {
-          chats.map((c) => (
-            <div className="message" key={c.id} 
+        {chats.map((c) => (
+          <div
+            className="message"
+            key={c.id}
             style={{
-              backgroundColor:
-                c.seenBy.includes(currentUser.id) ? "white" : "#fecd514e",
+              backgroundColor: c.seenBy.includes(currentUser.id)
+                ? "white"
+                : "#fecd514e",
             }}
             onClick={() => handleOpenChat(c.id, c.receiver)}
-            >
-          <img
-            src={c.receiver.avatar || "/noavatar.png"}
-            alt=""
-          />
-          <span>{c.receiver.username}</span>
-          <p>{c.lastMessage}</p>
-        </div>
-          ))
-        }
-       
+          >
+            <img src={c.receiver.avatar || "/noavatar.png"} alt="" />
+            <span>{c.receiver.username}</span>
+            <p>{c.lastMessage}</p>
+          </div>
+        ))}
       </div>
       {chat && (
         <div className="chatBox">
           <div className="top">
             <div className="user">
-              <img
-                src={chat.receiver.avatar || "/noavatar.png"}
-                alt=""
-              />
+              <img src={chat.receiver.avatar || "/noavatar.png"} alt="" />
               {chat.receiver.username}
             </div>
-            <span className="close" onClick={()=>setChat(null)}>X</span>
+            <span className="close" onClick={() => setChat(null)}>X</span>
           </div>
           <div className="center">
             {chat.messages.map((message) => (
-              <div className="chatMessage" 
-              style={{
-                  alignSelf:
-                    message.userId === currentUser.id
-                      ? "flex-end"
-                      : "flex-start",
-                  textAlign:
-                    message.userId === currentUser.id ? "right" : "left",
+              <div
+                className="chatMessage"
+                style={{
+                  alignSelf: message.userId === currentUser.id ? "flex-end" : "flex-start",
+                  textAlign: message.userId === currentUser.id ? "right" : "left",
                 }}
-              key={message.id}>
-              <p>{message.text}</p>
-              <span>{format(message.createdAt)}</span>
-            </div>
+                key={message.id}
+              >
+                <p>{message.text}</p>
+                <span>{format(message.createdAt)}</span>
+              </div>
             ))}
-            
           </div>
           <form onSubmit={handleSubmit} className="bottom">
             <textarea name="text"></textarea>
